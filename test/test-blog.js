df1dc3e501bb0d8f5b6c14f3a3bfda969468faf0
Jeff Connelly <jeff.connelly6@gmail.com>
2017-12-22 14:09:26 -0700

    Updated router/initialized test integration

---
+++ test/test-blog.js
@@ -0,0 +1,30 @@
+'use strict'; 
+
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+const {app, runServer, closeServer} = require('../server');
+
+const should = chai.should();
+chai.use(chaiHttp);
+
+describe('Blog', function() {
+
+  before(function() {
+    return runServer();
+  });
+
+  after(function() {
+    return closeServer();
+  });
+
+  it('should list items on GET', function() {
+    return chai.request(app)
+      .get('/blog-posts')
+      .then(function(res) {
+        res.should.have.status(200);
+        
+      });
+  });
+
+});
