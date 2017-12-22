3f27a378e94796bc7ac008ec1206a6f9f0dd1512
Jeff Connelly <jeff.connelly6@gmail.com>
2017-12-22 14:16:11 -0700

    Added GET Test functionality

--- test/test-blog.js
+++ test/test-blog.js
@@ -23,7 +23,16 @@ describe('Blog', function() {
       .get('/blog-posts')
       .then(function(res) {
         res.should.have.status(200);
-        
+        res.should.be.json;
+        res.body.should.be.a('array');
+        res.body.length.should.be.at.least(1);
+
+        const expectedKeys = ['title', 'content', 'author', 'id', 'publishDate'];
+
+        res.body.forEach(function (item) {
+          item.should.be.a('object');
+          item.should.include.keys(expectedKeys);
+        });
       });
   });
 
